c7d0427ce53aacb6b51808776788de2e41b52c3e
Yuliia Prunchak <d.deerter@gmail.com>
2026-02-02 20:26:59 +0100

    chore: create schema

---
+++ app/schema.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION OF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS categories (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name text NOT NULL UNIQUE,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
+CREATE TABLE IF NOT EXISTS tasks (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    title text NOT NULL,
+    description text,
+    status text NOT NULL DEFAULT "open" CHECK (status IN ('open', 'done', 'archived')),
+    priority int NOT NULL DEFAULT 3 CHECK (priority BETWEEN 1 and 5),
+    due_date date,
+    category_id uuid REFERENCES categories(id) ON DELETE SET NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now()
+);
