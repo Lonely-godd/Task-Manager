6af90a1749ff377c768b10a8a8785493d2e1fc68
Yuliia Prunchak <d.deerter@gmail.com>
2026-02-05 23:33:57 +0100

    search problem fixed

--- app/services/init_db.py
+++ app/services/init_db.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from app.db import db_cursor
 
 def apply_schema() -> None:
-    schema_path = Path(__file__).resole().parents(1)/ "schema.sql"
+    schema_path = Path(__file__).resolve().parent.parent / "schema.sql"
     sql = schema_path.read_text(encoding="utf-8")
 
     with db_cursor() as cur:
