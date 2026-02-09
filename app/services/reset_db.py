792884fd23727c7ec03231b1c2c3caab6be38ae7
Lonely-godd <d.deerter@gmail.com>
2026-02-09 23:46:50 +0100

    fixed tables reset

--- app/services/reset_db.py
+++ app/services/reset_db.py
@@ -4,5 +4,5 @@ from app.db import db_cursor
 
 def reset_tables() -> None:
     with db_cursor() as cur:
-        cur.execute("DROP TABLE IF EXISTS tasks CASCADE;")
-        cur.execute("DROP TABLE IF EXISTS categories CASCADE;")
+        cur.execute("TRUNCATE TABLE tasks CASCADE;")
+        cur.execute("TRUNCATE TABLE categories CASCADE;")
