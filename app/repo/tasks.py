fd9b9ea1391e54213f6bccc8639c72aeb7eb7b3e
Lonely-godd <d.deerter@gmail.com>
2026-02-11 22:59:53 +0100

    add description into creation of a task

--- app/repo/tasks.py
+++ app/repo/tasks.py
@@ -1,46 +1,47 @@
 from app.db import db_cursor
 
+
 def insert_task(
         title: str,
-        priority: str | None,
+        description: str | None,
+        priority: int | None,
         due_date: str | None,
 ) -> int:
     if priority is None:
         query = """
-            INSERT INTO tasks (title, priority, due_date)
-            VALUES (%s, 3, %s)
-            RETURNING id;
-        """
+                INSERT INTO tasks (title, description, priority, due_date)
+                VALUES (%s, %s, 3, %s) RETURNING id; \
+                """
         params = (title, due_date)
     else:
         query = """
-            INSERT INTO tasks (title, priority, due_date)
-            VALUES (%s, %s, %s)
-            RETURNING id;
-        """
-        params = (title, priority, due_date)
+                INSERT INTO tasks (title, description, priority, due_date)
+                VALUES (%s, %s, %s, %s) RETURNING id; \
+                """
+        params = (title, description, priority, due_date)
     with db_cursor() as cur:
         cur.execute(query, params)
         task_id = cur.fetchone()[0]
     return task_id
 
+
 def select_task(
-        status: str | None,
+        status: int | None,
 ):
     if status is None:
         query = """
-            SELECT id, title, status, priority, due_date, created_at
-            FROM tasks
-            ORDER BY created_at DESC;
-        """
+                SELECT id, title, description, status, priority, due_date, created_at
+                FROM tasks
+                ORDER BY created_at DESC; \
+                """
         params = ()
     else:
         query = """
-            SELECT id, title, status, priority, due_date, created_at
-            FROM tasks
-            WHERE status = %s
-            ORDER BY created_at DESC;
-        """
+                SELECT id, title, description, status, priority, due_date, created_at
+                FROM tasks
+                WHERE status = %s
+                ORDER BY created_at DESC; \
+                """
         params = (status,)
     with db_cursor() as cur:
         cur.execute(query, params)
